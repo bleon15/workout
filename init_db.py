60fe077519a9da5c1df60a69464908aa8d8a5be0
BLeon15 <165696136+BLeon15@users.noreply.github.com>
2025-04-16 21:47:47 -0400

    editing workout lists

--- init_db.py
+++ init_db.py
@@ -12,9 +12,15 @@ with open('db/schema.sql') as f:
 data = [
     ('legs', 'Squats', 3, 10),
     ('legs', 'Lunges', 3, 12),
+    ('legs', 'Goblin Squats', 3, 10),
     ('chest', 'Bench Press', 3, 10),
+    ('chest', 'Chest Fly', 3, 12),
+    ('chest', 'Incline Dumbell Press', 4, 8),
     ('arms', 'Bicep Curls', 2, 15),
+    ('arms', 'Hammer Curls', 2, 15),
+    ('arms', 'Preacher Curls', 2, 15),
     ('cardio', 'Running', 1, 30)
+    ('cardio', 'Sprints', 5, 10)
 ]
 conn.executemany("INSERT INTO exercises (type, name, sets, reps) VALUES (?, ?, ?, ?)", data)
 
